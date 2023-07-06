42eb0e1af9595a3f8fa2709183887a273b428615
Aditya Kandpal <120099176+aditya955569@users.noreply.github.com>
2023-07-06 13:43:10 +0530

    funequations.m

--- Indeterminate_Beams2/funequations.m
+++ Indeterminate_Beams2/funequations.m
@@ -2,12 +2,12 @@ function equations=funequations(length_parts,count)
 for i=1:1:count
     for j=1:1:count
         if j==i
-            equations(i,j)=2*(1/length_parts(1,i))+2*(1/length_parts(1,i+1));
+            equations(i,j)=4*(1/length_parts(1,i))+4*(1/length_parts(1,i+1));
             if(j-1>=1)
-                equations(i,j-1)=1/length_parts(1,i);
+                equations(i,j-1)=2*(1/length_parts(1,i));
             end
         elseif j==i+1
-            equations(i,j)=1/length_parts(1,i+1);
+            equations(i,j)=2*(1/length_parts(1,i+1));
         else
             equations(i,j)=0;
         end
