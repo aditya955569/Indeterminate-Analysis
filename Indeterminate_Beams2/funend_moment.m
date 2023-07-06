a7e08c08ec63af368f0930d8ee910437c1d8ff56
Aditya Kandpal <120099176+aditya955569@users.noreply.github.com>
2023-07-06 15:10:44 +0530

    funend_moment.m

--- Indeterminate_Beams2/funend_moment.m
+++ Indeterminate_Beams2/funend_moment.m
@@ -1,9 +1,9 @@
 function end_moment=funend_moment(udl ,length_parts,theta_want,count)
-    end_moment(1)=(-udl*length_parts(1)*length_parts(1))/12+(2*theta_want(1))/length_parts(1);
-    end_moment(2)=(udl*length_parts(2)*length_parts(2))/12+(2*theta_want(1))/length_parts(2)+theta_want(2)/length_parts(2);
+    end_moment(1)=(-udl*length_parts(1)*length_parts(1))/12+(4*theta_want(1))/length_parts(1);
+    end_moment(2)=(udl*length_parts(2)*length_parts(2))/12+(4*theta_want(1))/length_parts(2)+2*theta_want(2)/length_parts(2);
    for i=3:2:2*count
     p=(i+1)/2;
-    end_moment(i)=(-udl*length_parts(p)*length_parts(p))/12+2*theta_want(p)/length_parts(p)+theta_want(p-1)/length_parts(p);
-    end_moment(i+1)=(udl*length_parts(p+1)*length_parts(p+1))/12+2*theta_want(p)/length_parts(p+1)+theta_want(p+1)/length_parts(p+1);
+    end_moment(i)=(-udl*length_parts(p)*length_parts(p))/12+4*theta_want(p)/length_parts(p)+2*theta_want(p-1)/length_parts(p);
+    end_moment(i+1)=(udl*length_parts(p+1)*length_parts(p+1))/12+4*theta_want(p)/length_parts(p+1)+2*theta_want(p+1)/length_parts(p+1);
    end
 end
